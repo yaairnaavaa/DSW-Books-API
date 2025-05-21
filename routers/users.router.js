7a399193060cc20da515509b9979bd67c68ef7ad
Yair Nava <iryanavahe@ittepic.edu.mx>
2025-05-21 16:33:46 -0700

    feat: add authMiddleware in get users route

--- routers/users.router.js
+++ routers/users.router.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/users.controller');
 const authMiddleware = require('../utils/auth.middleware');
 
 router.post('/', userController.registerUser);
-router.get('/', userController.getUsers);
+router.get('/', authMiddleware.authenticateToken, userController.getUsers);
 router.post('/login', userController.loginUser);
 
 module.exports = router;
